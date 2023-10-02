28010efbd98524bc40747f9d3078e2cfb42984cb
bianca <bianca.milanez@kcl.ac.uk>
2023-10-03 00:38:32 +0100

    First Attempt

--- primes.py
+++ primes.py
@@ -2,5 +2,24 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <=0:
+        raise ValueError("invalid number typed")
+    
     list = []
+    counter = 2
+    while len(list) < number_of_primes:
+        divisible = False
+    
+        for i in range(counter, 1, -1):
+            if counter%i==0 and counter != i:
+                divisible=True
+                break
+
+        if (not divisible):
+            list.append(counter)
+
+        counter+= 1
+        
     return list
+
+primes(1)
